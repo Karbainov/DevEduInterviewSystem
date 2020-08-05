6b9d1ed4730a813cb292da1933ec32f6eb079494
Anastasia <anastasila.mik@yandex.ru>
2020-08-05 21:23:53 +0300

    Add Candidate Table

--- DevEduInterviewSystem.DataBase/Tables/Candidate.sql
+++ DevEduInterviewSystem.DataBase/Tables/Candidate.sql
@@ -1,4 +1,11 @@
-﻿CREATE TABLE [dbo].[Table1]
-(
-	[Id] INT NOT NULL PRIMARY KEY
+﻿CREATE TABLE dbo.[Candidate] (
+	ID int Primary key Identity,
+	StageID int FOREIGN KEY ([StageID]) REFERENCES [Stage]([ID]) NULL,
+	StatusID int FOREIGN KEY ([StatusID]) REFERENCES [Status]([ID]) NULL,
+	CityID int FOREIGN KEY ([CityID]) REFERENCES [City]([ID]) NULL,
+	Phone int NULL,
+	Email nvarchar(30) NULL,
+	FirstName nvarchar(30) NULL,
+	LastName nvarchar(30) NULL,
+	Birthday datetime2 NULL
 )
