deb06b042a31d4f31d7cfb0fa370162d6e9d5199
Aleksey <americos2007@yandex.ru>
2020-08-05 20:57:30 +0300

    Add table StageChenged

--- DevEduInterviewSystem.DataBase/StoredProcedures/CRUD/StageChangedProcedure/DeleteStageChangedByID.sql
+++ DevEduInterviewSystem.DataBase/StoredProcedures/CRUD/StageChangedProcedure/DeleteStageChangedByID.sql
@@ -1,6 +1,4 @@
 ﻿CREATE PROCEDURE [dbo].[DeleteStageChangedByID]
-	@param1 int = 0,
-	@param2 int
+@ID int 
 AS
-	SELECT @param1, @param2
-RETURN 0
+Delete from [dbo].[StageChanged] where (@ID = ID)
--- DevEduInterviewSystem.DataBase/Tables/StageChanged.sql
+++ DevEduInterviewSystem.DataBase/Tables/StageChanged.sql
@@ -1,7 +1,7 @@
 ﻿CREATE TABLE [dbo].[StageChanged]
 (
 	[Id] INT NOT NULL PRIMARY KEY,
-	CandidateID int NULL,   --FOREIGN KEY ([dbo].[CandidateID]) REFERENCES [dbo].[Candidate]([ID])						
-	StageID int NULL,		--FOREIGN KEY ([dbo].[StageID]) REFERENCES [dbo].[Stage]([ID])
+	[CandidateID] int NULL,   --FOREIGN KEY ([dbo].[CandidateID]) REFERENCES [dbo].[Candidate]([ID])						
+	[StageID] int NULL,		--FOREIGN KEY ([dbo].[StageID]) REFERENCES [dbo].[Stage]([ID])
 	[ChangedDate] datetime2 NULL,
 )
