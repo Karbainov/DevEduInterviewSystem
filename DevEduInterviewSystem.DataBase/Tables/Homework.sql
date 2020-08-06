3f82ab544485920493a9fb1785ed720b90fed661
Mikhail <gooddok99@gmail.com>
2020-08-07 00:05:58 +0300

    Edited FOREIGN KEY

--- DevEduInterviewSystem.DataBase/Tables/Homework.sql
+++ DevEduInterviewSystem.DataBase/Tables/Homework.sql
@@ -2,8 +2,8 @@
 (
 
 	ID int Primary key Identity,
-	CandidateID int FOREIGN KEY ([CandidateID]) REFERENCES [Candidate]([ID]) NULL,
-	HomeworkStatusID int FOREIGN KEY ([HomeworkStatusID]) REFERENCES [HomeworkStatus]([ID]) NULL,
+	CandidateID int  NULL FOREIGN KEY ([CandidateID]) REFERENCES [Candidate]([ID]),
+	HomeworkStatusID int NULL FOREIGN KEY ([HomeworkStatusID]) REFERENCES [HomeworkStatus]([ID]) ,
 	TestStatusID int NULL,
 	HomeworkDate datetime2 NULL
 	
