a5ed487ea1e8e8c8130be0cb11bb2abd78fbc7bc
Denis <denis.matveev@yahoo.com>
2020-08-06 22:12:07 +0300

    add References Course_Candidate

--- DevEduInterviewSystem.DataBase/Tables/Course_Candidate.sql
+++ DevEduInterviewSystem.DataBase/Tables/Course_Candidate.sql
@@ -1,5 +1,5 @@
 ﻿CREATE TABLE [dbo].[Course_Candidate] ( 
 	ID int NOT NULL PRIMARY KEY IDENTITY, 
-	CourseID int NOT NULL,
-	CandidateID int NOT NULL
+	[CourseID] int NOT NULL FOREIGN KEY ([CourseID]) REFERENCES [dbo].[Course]([ID]),
+	CandidateID int NOT NULL FOREIGN KEY (CandidateID) REFERENCES [dbo].[Candidate]([ID])
 )
