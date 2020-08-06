1f159e22165ffffa41a2f615fa659a3018e21caf
Daramir95 <daramir1995@gmail.com>
2020-08-07 00:01:12 +0300

    Edit Code

--- DevEduInterviewSystem.DataBase/Tables/Interview.sql
+++ DevEduInterviewSystem.DataBase/Tables/Interview.sql
@@ -1,7 +1,7 @@
 ﻿CREATE TABLE [dbo].[Interview] (
 	ID int Primary key Identity,
-	CandidateID int NULL, --FOREIGN KEY ([CandidateID]) REFERENCES [Candidate]([ID])
-	InterviewStatusID int NULL, --FOREIGN KEY ([InterviewStatusID]) REFERENCES [InterviewStatus]([ID])
+	CandidateID int NULL FOREIGN KEY ([CandidateID]) REFERENCES [Candidate]([ID]),
+	InterviewStatusID int NULL FOREIGN KEY ([InterviewStatusID]) REFERENCES [InterviewStatus]([ID]),
 	Attempt int NULL,
 	DateTimeInterview datetime2 NULL,
  )
