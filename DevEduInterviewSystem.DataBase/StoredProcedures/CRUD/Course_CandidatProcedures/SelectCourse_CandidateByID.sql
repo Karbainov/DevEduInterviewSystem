7c6b2dcff9937bebbd394d6f6d45563d9bfcab27
Denis <denis.matveev@yahoo.com>
2020-08-06 20:00:21 +0300

    Rebuild table Course_Candidate and procedures

---
+++ DevEduInterviewSystem.DataBase/StoredProcedures/CRUD/Course_CandidatProcedures/SelectCourse_CandidateByID.sql
@@ -0,0 +1,5 @@
+﻿CREATE PROCEDURE [dbo].[SelectCourse_CandidateByID]
+	@ID int 
+AS
+Select  * From [dbo].[Course_Candidate] 
+where (@ID = ID)
---
+++ DevEduInterviewSystem.DataBase/Tables/Course_Candidate.sql
@@ -0,0 +1,5 @@
+﻿CREATE TABLE [dbo].[Course_Candidate] ( 
+	ID int NOT NULL PRIMARY KEY IDENTITY, 
+	CourseID int NOT NULL,
+	CandidateID int NOT NULL
+)
