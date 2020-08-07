369d552c615bb57b008c797cd85e734dac79e445
Daramir95 <daramir1995@gmail.com>
2020-08-07 13:41:41 +0300

    Edit Code

--- DevEduInterviewSystem.DataBase/Tables/CandidatePersonalInfo.sql
+++ DevEduInterviewSystem.DataBase/Tables/CandidatePersonalInfo.sql
@@ -1,11 +1,11 @@
-﻿CREATE TABLE dbo.[CandidatePersonalInfo] (
-	ID int Primary key Identity,
-	CandidateID int FOREIGN KEY ([CandidateID]) REFERENCES [Candidate]([ID]) NULL,
-	MaritalStatus bit NULL,
-	Education nvarchar(50) NULL,
-	WorkPlace nvarchar(50) NULL,
-	ITExperience nvarchar(50) NULL,
-	Hobbies nvarchar(50) NULL,
-	InfoSourse nvarchar(50) NULL,
-	Expectations nvarchar(50) NULL
+﻿CREATE TABLE [dbo].[CandidatePersonalInfo] (
+	[ID] int Primary key Identity,
+	[CandidateID] int FOREIGN KEY ([CandidateID]) REFERENCES [Candidate]([ID]) NULL,
+	[MaritalStatus] bit NULL,
+	[Education] nvarchar(50) NULL,
+	[WorkPlace] nvarchar(50) NULL,
+	[ITExperience] nvarchar(50) NULL,
+	[Hobbies] nvarchar(50) NULL,
+	[InfoSourse] nvarchar(50) NULL,
+	[Expectations] nvarchar(50) NULL
 )
