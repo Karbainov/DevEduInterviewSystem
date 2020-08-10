24cc88b48f49e8a910ca6abc59b6e27c2077e59f
Mikhail <gooddok99@gmail.com>
2020-08-10 03:33:52 +0300

    Added HomeworkDTO

---
+++ DevEduInterviewSystem.DAL/DTO/HomeworkDTO.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DevEduInterviewSystem.DAL.DTO
+{
+    public class HomeworkDTO
+    {
+        public int ID { get; set; }
+        public int CandidateID { get; set; }
+        public int HomeworkStatusID { get; set; }
+        public int TestStatusID { get; set; }
+        public DateTime HomeworkDate { get; set; }
+
+        public HomeworkDTO()
+        {
+
+        }
+
+        public HomeworkDTO(int ID, int CandidateID, int HomeworkStatusID, int TestStatusID, DateTime HomeworkDate)
+        {
+            this.ID = ID;
+            this.CandidateID = CandidateID;
+            this.HomeworkStatusID = HomeworkStatusID;
+            this.TestStatusID = TestStatusID;
+            this.HomeworkDate = HomeworkDate;
+
+        }
+    }
+}
+
