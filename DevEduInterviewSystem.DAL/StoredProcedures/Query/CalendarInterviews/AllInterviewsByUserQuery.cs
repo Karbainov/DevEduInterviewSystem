32730e377eb1e342bd0a7d399c7385537b9fe375
Denis <denis.matveev@yahoo.com>
2020-08-11 21:16:12 +0300

    fix SelectAllInterviewByUser

--- DevEduInterviewSystem.DAL/StoredProcedures/Query/CalendarInterviews/AllInterviewsByUserQuery.cs
+++ DevEduInterviewSystem.DAL/StoredProcedures/Query/CalendarInterviews/AllInterviewsByUserQuery.cs
@@ -11,11 +11,12 @@ namespace DevEduInterviewSystem.DAL.StoredProcedures.Query.CalendarInterviews
     public class AllInterviewsByUserQuery
     {
         SqlConnection connection = new SqlConnection(PrimerConnection.ConnectionString);
-        public List<AllInterviewsByUserDTO> SelectAll()
+        public List<AllInterviewsByUserDTO> SelectAllByUser(int id)
         {
             connection.Open();
             SqlCommand command = ReferenceToProcedure("AllInterviewsByUser", connection);
-
+            SqlParameter userParam = new SqlParameter("@UserID", id);
+            command.Parameters.Add(userParam);
             SqlDataReader reader = command.ExecuteReader();
 
             List<AllInterviewsByUserDTO> interviews = new List<AllInterviewsByUserDTO>();
@@ -26,15 +27,15 @@ namespace DevEduInterviewSystem.DAL.StoredProcedures.Query.CalendarInterviews
                 {
                     AllInterviewsByUserDTO interview = new AllInterviewsByUserDTO()
                     {
-                        UserFirstName = (string)reader["U.[FirstName]"],
-                        UserLastName = (string)reader["U.[LastName]"],
-                        IDCandidate = (int)reader["C.[ID]"],
-                        CandidateFirstName = (string)reader["C.[FirstName]"],
-                        CandidateLastName = (string)reader["C.[LastName]"],
-                        CandidatePhone = (string)reader["C.[Phone]"],
-                        DateTimeInterview = (DateTime)reader["I.[DateTimeInterview]"],
-                        Attempt = (int)reader["I.[Attempt]"],
-                        InterviewStatus = (string)reader["ISt.[Name]"]
+                        UserFirstName = (string)reader["FirstName"],
+                        UserLastName = (string)reader["LastName"],
+                        IDCandidate = (int)reader["ID"],
+                        CandidateFirstName = (string)reader["FirstName"],
+                        CandidateLastName = (string)reader["LastName"],
+                        CandidatePhone = (string)reader["Phone"],
+                        DateTimeInterview = (DateTime)reader["DateTimeInterview"],
+                        Attempt = (int)reader["Attempt"],
+                        InterviewStatus = (string)reader["Name"]
                     };
                     interviews.Add(interview);
                 }
