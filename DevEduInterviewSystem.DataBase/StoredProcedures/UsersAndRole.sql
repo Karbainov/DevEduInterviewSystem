08ed13c1a7b1f4ffd2890db04691f0d6a0a3a01a
Daramir95 <daramir1995@gmail.com>
2020-08-11 20:41:38 +0300

    edit select

--- DevEduInterviewSystem.DataBase/StoredProcedures/UsersAndRole.sql
+++ DevEduInterviewSystem.DataBase/StoredProcedures/UsersAndRole.sql
@@ -1,6 +1,6 @@
 ﻿CREATE PROCEDURE [dbo].[UsersAndRole] AS
 
 	SELECT U.Login, U.FirstName, U.LastName, R.TypeOfRole FROM User_Role AS UR
-	INNER JOIN [User] AS U ON U.ID = UR.ID
+	INNER JOIN [User] AS U ON U.ID = UR.UserID
 	INNER JOIN [Role] AS R ON R.ID = UR.RoleID
 
