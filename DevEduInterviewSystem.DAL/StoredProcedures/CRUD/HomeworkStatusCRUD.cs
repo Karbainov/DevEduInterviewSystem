1278a901e161fa9feaf9614e5740ad511413c3ec
AndreyKarlukov <karl911291@yandex.ru>
2020-08-11 12:58:21 +0300

    Fix homeworkStatus

--- DevEduInterviewSystem.DAL/StoredProcedures/CRUD/HomeworkStatusCRUD.cs
+++ DevEduInterviewSystem.DAL/StoredProcedures/CRUD/HomeworkStatusCRUD.cs
@@ -7,109 +7,101 @@ using System.Text;
 
 namespace DevEduInterviewSystem.DAL.StoredProcedures.CRUD
 {
-    public class HomeworkStatusCRUD
+    public class HomeworkStatusCRUD : AbstractCRUD<HomeworkStatusDTO>
     {
-        public int Add(SqlConnection connection, HomeworkStatusDTO homeworkStatus)
+        public override int Add(HomeworkStatusDTO dto)
         {
-            connection.Open();
-            SqlCommand command = ReferenceToProcedure("AddHomeworkStatus", connection);
+            Connection.Open();
+            SqlCommand command = ReferenceToProcedure("AddHomeworkStatus", Connection);
 
-            SqlParameter NameParam = new SqlParameter("@Name", homeworkStatus.Name);
+            SqlParameter NameParam = new SqlParameter("@Name", dto.Name);
             command.Parameters.Add(NameParam);
 
             return command.ExecuteNonQuery();
         }
 
-        public int DeleteByID(SqlConnection connection, int ID)
+        public override int DeleteByID(int id)
         {
-            connection.Open();
+            Connection.Open();
             string sqlExpression = "DeleteHomeworkStatusByID";
-            SqlCommand command = new SqlCommand(sqlExpression, connection);
+            SqlCommand command = new SqlCommand(sqlExpression, Connection);
             command.CommandType = System.Data.CommandType.StoredProcedure;
 
-            SqlParameter IDParam = new SqlParameter("@ID", ID);
+            SqlParameter IDParam = new SqlParameter("@ID", id);
             command.Parameters.Add(IDParam);
 
             return command.ExecuteNonQuery();
         }
 
-        public int SelectAll(SqlConnection connection)
+
+        public override List<HomeworkStatusDTO> SelectAll()
         {
-            connection.Open();
-            SqlCommand command = ReferenceToProcedure("SelectAllHomeworkStatus", connection);
+            Connection.Open();
+            SqlCommand command = ReferenceToProcedure("SelectAllHomeworkStatus", Connection);
 
             SqlDataReader reader = command.ExecuteReader();
 
+            List<HomeworkStatusDTO> homeworktatuses = new List<HomeworkStatusDTO>();
+
             if (reader.HasRows) // если есть данные
             {
-                // выводим названия столбцов
-                Console.WriteLine($"id \t Name");
+                while (reader.Read()) // построчно считываем данные
+                {
+                    HomeworkStatusDTO homeworkStatus = new HomeworkStatusDTO()
+                    {
+                        ID = (int)reader["id"],
+                        Name = (string)reader["Name"]
+                    };
+
+                    homeworktatuses.Add(homeworkStatus);
+                }
+            }
+            reader.Close();
+            return homeworktatuses;
+        }
+
+
+        public override HomeworkStatusDTO SelectByID(int id)
+        {
+            Connection.Open();
+            SqlCommand command = ReferenceToProcedure("SelectHomeworkStatusByID", Connection);
+
+            SqlParameter IDParam = new SqlParameter("@ID", id);
+            command.Parameters.Add(IDParam);
+
+            SqlDataReader reader = command.ExecuteReader();
+            HomeworkStatusDTO homeworkStatus = new HomeworkStatusDTO();
+
+            if (reader.HasRows) // если есть данные
+            {
 
                 while (reader.Read()) // построчно считываем данные
                 {
-                    int id = (int)reader["id"];
-                    string Name = (string)reader["Name"];
-
-
-                    Console.WriteLine($"{id} \t{Name} ");
+                    homeworkStatus.ID = (int)reader["id"];
+                    homeworkStatus.Name = (string)reader["Name"];
                 }
             }
             reader.Close();
 
-            return (int)command.ExecuteScalar();
+            return homeworkStatus;
         }
 
-        public int SelectByID(SqlConnection connection, int ID)
-        {
-            connection.Open();
-            SqlCommand command = ReferenceToProcedure("SelectHomeworkStatusByID", connection);
 
-            SqlParameter IDParam = new SqlParameter("@ID", ID);
+        public override int UpdateByID(HomeworkStatusDTO dto)
+        {
+
+            Connection.Open();
+            SqlCommand command = ReferenceToProcedure("UpdateHomeworkStatusByID", Connection);
+
+            SqlParameter IDParam = new SqlParameter("@ID", dto.ID);
             command.Parameters.Add(IDParam);
 
-            SqlDataReader reader = command.ExecuteReader();
-
-            if (reader.HasRows) // если есть данные
-            {
-                // выводим названия столбцов
-                Console.WriteLine($"id \t Name");
-
-                while (reader.Read()) // построчно считываем данные
-                {
-                    int id = (int)reader["id"];
-                    string Name = (string)reader["Name"];
-
-
-                    Console.WriteLine($"{id} \t{Name} ");
-                }
-            }
-            reader.Close();
-
-            return (int)command.ExecuteScalar();
-        }
-        public int UpdateByID(SqlConnection connection, HomeworkStatusDTO homeworkStatus, int ID)
-        {
-            connection.Open();
-            SqlCommand command = ReferenceToProcedure("UpdateHomeworkStatusByID", connection);
-
-            SqlParameter IDParam = new SqlParameter("@ID", ID);
-            command.Parameters.Add(IDParam);
-
-            SqlParameter NameParam = new SqlParameter("@Name", homeworkStatus.Name);
+            SqlParameter NameParam = new SqlParameter("@Name", dto.Name);
             command.Parameters.Add(NameParam);
 
 
 
             return command.ExecuteNonQuery();
-        }
-
-
-        private SqlCommand ReferenceToProcedure(string sqlExpression, SqlConnection connection)
-        {
-            SqlCommand command = new SqlCommand(sqlExpression, connection);
-            command.CommandType = System.Data.CommandType.StoredProcedure;
-
-            return command;
         }
     }
 }
