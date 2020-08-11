01b013d204b957e90d508c0883616e62e8e3910e
kish-anna <annykulebiakina@gmail.com>
2020-08-11 23:29:16 +0300

    Add query and DTO

---
+++ DevEduInterviewSystem.DAL/DTO/CalendarInterviews/AllInterviewsByDateDTO.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DevEduInterviewSystem.DAL.DTO.CalendarInterviews
+{
+    public class AllInterviewsByDateDTO : IDTO
+    {
+        public string UserFirstName { get; set; }
+        public string UserLastName { get; set; }
+        public int IDCandidate { get; set; }
+        public string CandidateFirstName { get; set; }
+        public string CandidateLastName { get; set; }
+        public string CandidatePhone { get; set; }
+        public DateTime DateTimeInterview { get; set; }
+        public int Attempt { get; set; }
+        public string InterviewStatus { get; set; }
+
+        public AllInterviewsByDateDTO()
+        {
+
+        }
+        public AllInterviewsByDateDTO(string userFirstName, string userLastName, int idCandidate, string candidateFirstName,
+            string candidateLastName, string candidatePhone, DateTime dateTimeInterview, int attempt, string interviewStatus)
+        {
+            UserFirstName = userFirstName;
+            UserLastName = userLastName;
+            IDCandidate = idCandidate;
+            CandidateFirstName = candidateFirstName;
+            CandidateLastName = candidateLastName;
+            CandidatePhone = candidatePhone;
+            DateTimeInterview = dateTimeInterview;
+            Attempt = attempt;
+            InterviewStatus = interviewStatus;
+
+        }
+    }
+}
