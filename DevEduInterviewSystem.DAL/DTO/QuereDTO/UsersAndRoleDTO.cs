b7ebd369172f217a7da994b172916e5acb6f2147
Daramir95 <daramir1995@gmail.com>
2020-08-11 18:05:35 +0300

    Add request UsersAndRole

---
+++ DevEduInterviewSystem.DAL/DTO/QuereDTO/UsersAndRoleDTO.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DevEduInterviewSystem.DAL.DTO.QuereDTO
+{
+    public class UsersAndRoleDTO
+    {
+        public string Login { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Role { get; set; }
+
+        public UsersAndRoleDTO()
+        {
+
+        }
+
+        public UsersAndRoleDTO(string login, string name, string lastName, string role)
+        {
+            Login = login;
+            FirstName = name;
+            LastName = lastName;
+            Role = role;
+        }
+    }
+}
