15f1061a8af5436e5b41a7896ed082601511d7d1
Di <diana.iancheva@gmail.com>
2020-08-13 23:29:38 +0300

    Added all tasks by user

---
+++ DevEduInterviewSystem.DAL/DTO/QueryDTO/AllTasksByUserDTO.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DevEduInterviewSystem.DAL.DTO.QueryDTO
+{
+    public class AllTasksByUserDTO
+    {
+        public string UserFirstName { get; set; }
+        public string UserLastName { get; set; }
+        public int? CandidateID { get; set; }
+        public string CandidateFirstName { get; set; }
+        public string CandidateLastName { get; set; }
+        public string Task { get; set; }
+        public bool? IsCompleted { get; set; }
+        public string Stage { get; set; }
+
+        public AllTasksByUserDTO() { }
+
+        public AllTasksByUserDTO(string userFirstName, string userLastName, int candidateID, string candidateFirstName,
+            string candidateLastName, string task, bool isCompleted, string stage)
+        {
+            UserFirstName = userFirstName;
+            UserLastName = userLastName;
+            CandidateID = candidateID;
+            CandidateFirstName = candidateFirstName;
+            CandidateLastName = candidateLastName;
+            Task = task;
+            IsCompleted = isCompleted;
+            Stage = stage;
+        }
+    }
+}
---
+++ DevEduInterviewSystem.DAL/StoredProcedures/Query/AllTasksByUserQuery.cs
@@ -0,0 +1,53 @@
+﻿using DevEduInterviewSystem.DAL.DTO.QueryDTO;
+using DevEduInterviewSystem.DAL.Shared;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Text;
+
+namespace DevEduInterviewSystem.DAL.StoredProcedures.Query
+{
+    public class AllTasksByUserQuery
+    {
+        public List<AllTasksByUserDTO> SelectAllTasksByUser(int id)
+        {
+            SqlConnection connection = ConnectionSingleTone.GetInstance().Connection;
+            connection.Open();
+            SqlCommand command = ReferenceToProcedure("AllTasksByUser", connection);
+            SqlParameter userParam = new SqlParameter("@UserID", id);
+            command.Parameters.Add(userParam);
+            SqlDataReader reader = command.ExecuteReader();
+
+            List<AllTasksByUserDTO> tasks = new List<AllTasksByUserDTO>();
+
+            if (reader.HasRows)
+            {
+                while (reader.Read())
+                {
+                    AllTasksByUserDTO task = new AllTasksByUserDTO()
+                    {
+                        UserFirstName = (string)reader["UserFirstName"],
+                        UserLastName = (string)reader["UserLastName"],
+                        CandidateID = (int)reader["CandidateID"],
+                        CandidateFirstName = (string)reader["CandidateFirstName"],
+                        CandidateLastName = (string)reader["CandidateLastName"],
+                        Task = (string)reader["Task"],
+                        IsCompleted = (bool)reader["IsCompleted"],
+                        Stage = (string)reader["Stage"]
+                    };
+                    tasks.Add(task);
+                }
+            }
+            reader.Close();
+
+            return tasks;
+        }
+        private SqlCommand ReferenceToProcedure(string sqlExpression, SqlConnection connection)
+        {
+            SqlCommand command = new SqlCommand(sqlExpression, connection);
+            command.CommandType = System.Data.CommandType.StoredProcedure;
+
+            return command;
+        }
+    }
+}
