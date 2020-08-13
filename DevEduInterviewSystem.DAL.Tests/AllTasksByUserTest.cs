9dc25fea28156b20ee8809c040b5231e121ddcba
Di <diana.iancheva@gmail.com>
2020-08-14 00:05:09 +0300

    Added task dto mock

---
+++ DevEduInterviewSystem.DAL.Tests/AllTasksByUserTest.cs
@@ -0,0 +1,32 @@
+﻿using DevEduInterviewSystem.DAL.DTO;
+using DevEduInterviewSystem.DAL.DTO.CalendarInterviews;
+using DevEduInterviewSystem.DAL.Shared;
+using DevEduInterviewSystem.DAL.StoredProcedures.CRUD;
+using DevEduInterviewSystem.DAL.StoredProcedures.Query.CalendarInterviews;
+using NUnit.Framework;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+
+namespace DevEduInterviewSystem.DAL.Tests
+{
+    [TestFixture]
+    public class AllTasksByUserTest
+    {
+        private List<int> _mockUserID;
+        private List<int> _mockTaskID;
+        private List<int> _mockCandidateID;
+        SqlConnection Connection;
+
+        [SetUp]
+        public void Setup()
+        {
+            Connection = ConnectionSingleTone.GetInstance().Connection;
+            _mockUserID = new List<int>();
+            _mockTaskID = new List<int>();
+            _mockCandidateID = new List<int>();
+
+        }
+    }
+}
---
+++ DevEduInterviewSystem.DAL.Tests/Mocks/TaskDTOMock.cs
@@ -0,0 +1,18 @@
+﻿using DevEduInterviewSystem.DAL.DTO;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+namespace DevEduInterviewSystem.DAL.Tests.Mocks
+{
+    public class TaskDTOMock : IEnumerable
+    {
+        public IEnumerator GetEnumerator()
+        {
+            yield return new TaskDTO(1, 2, 1, "Check HW", "1");
+            yield return new TaskDTO(2, 1, 2, "Check Test", "1");
+            yield return new TaskDTO(3, 4, 3, "Send Feedback", "1");
+            yield return new TaskDTO(4, 3, 4, "Call back", "1");
+        }
+    }
+}
