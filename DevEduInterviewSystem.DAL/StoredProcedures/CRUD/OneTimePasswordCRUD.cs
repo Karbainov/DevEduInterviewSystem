59323f426824276a394e013209b987ca3937c7e6
Di <diana.iancheva@gmail.com>
2020-08-14 16:51:33 +0300

    Added one time password crud

--- DevEduInterviewSystem.DAL/StoredProcedures/CRUD/OneTimePasswordCRUD.cs
+++ DevEduInterviewSystem.DAL/StoredProcedures/CRUD/OneTimePasswordCRUD.cs
@@ -13,11 +13,17 @@ namespace DevEduInterviewSystem.DAL.StoredProcedures.CRUD
             Connection.Open();
             SqlCommand command = ReferenceToProcedure("AddOneTimePassword");
 
-            SqlParameter TypeOfRoleParam = new SqlParameter("@TypeOfRole", dto.TypeOfRole);
-            command.Parameters.Add(TypeOfRoleParam);
+            SqlParameter CandidateIDParam = new SqlParameter("@CandidateID", dto.CandidateID);
+            command.Parameters.Add(CandidateIDParam);
+
+            SqlParameter DateOfPasswordIssueParam = new SqlParameter("@DateOfPasswordIssue", dto.DateOfPasswordIssue);
+            command.Parameters.Add(DateOfPasswordIssueParam);
+
+            SqlParameter OneTimePasswordParam = new SqlParameter("@OneTimePassword", dto.OneTimePassword);
+            command.Parameters.Add(OneTimePasswordParam);
 
             command.ExecuteNonQuery();
-            SqlCommand returnCurrentID = new SqlCommand("SELECT MAX([ID]) FROM dbo.[Role]", Connection);
+            SqlCommand returnCurrentID = new SqlCommand("SELECT MAX([ID]) FROM dbo.[OneTimePassword]", Connection);
             int count = (int)returnCurrentID.ExecuteScalar();
 
             Connection.Close();
@@ -27,22 +33,90 @@ namespace DevEduInterviewSystem.DAL.StoredProcedures.CRUD
 
         public override int DeleteByID(int id)
         {
-            throw new NotImplementedException();
+            Connection.Open();
+            SqlCommand command = ReferenceToProcedure("OneTimePasswordByID");
+
+            SqlParameter IDParam = new SqlParameter("@ID", id);
+            command.Parameters.Add(IDParam);
+
+            int a = command.ExecuteNonQuery();
+            Connection.Close();
+            return a;
         }
 
         public override List<OneTimePasswordDTO> SelectAll()
         {
-            throw new NotImplementedException();
+            Connection.Open();
+            SqlCommand command = ReferenceToProcedure("SelectAllOneTimePassword");
+            SqlDataReader reader = command.ExecuteReader();
+            List<OneTimePasswordDTO> passwords = new List<OneTimePasswordDTO>();
+            if (reader.HasRows)
+            {
+                while (reader.Read())
+                {
+                    OneTimePasswordDTO password = new OneTimePasswordDTO()
+                    {
+                        ID = (int)reader["id"],
+                        CandidateID = (int)reader["CandidateID"],
+                        DateOfPasswordIssue = (DateTime)reader["DateOfPasswordIssue"],
+                        OneTimePassword = (string)reader["OneTimePassword"]
+                    };
+                    passwords.Add(password);
+                }
+            }
+            reader.Close();
+            Connection.Close();
+            return passwords;
         }
 
         public override OneTimePasswordDTO SelectByID(int id)
         {
-            throw new NotImplementedException();
+            Connection.Open();
+            SqlCommand command = ReferenceToProcedure("SelectOneTimePasswordByID");
+
+            SqlParameter IDParam = new SqlParameter("@ID", id);
+            command.Parameters.Add(IDParam);
+
+            SqlDataReader reader = command.ExecuteReader();
+
+            OneTimePasswordDTO password = new OneTimePasswordDTO();
+
+            if (reader.HasRows)
+            {
+                while (reader.Read())
+                {
+                    password.ID = (int)reader["id"];
+                    password.CandidateID = (int)reader["CandidateID"];
+                    password.DateOfPasswordIssue = (DateTime)reader["DateOfPasswordIssue"];
+                    password.OneTimePassword = (string)reader["OneTimePassword"];
+                }
+            }
+            reader.Close();
+            Connection.Close();
+            return password;
         }
 
         public override int UpdateByID(OneTimePasswordDTO dto)
         {
-            throw new NotImplementedException();
+            Connection.Open();
+            SqlCommand command = ReferenceToProcedure("UpdateOneTimePasswordByID");
+
+            SqlParameter IDParam = new SqlParameter("@ID", dto.ID);
+            command.Parameters.Add(IDParam);
+
+            SqlParameter CandidateParam = new SqlParameter("@CandidateID", dto.CandidateID);
+            command.Parameters.Add(CandidateParam);
+
+            SqlParameter DateOfPasswordIssueParam = new SqlParameter("@DateOfPasswordIssue", dto.DateOfPasswordIssue);
+            command.Parameters.Add(DateOfPasswordIssueParam);
+
+            SqlParameter OneTimePasswordParam = new SqlParameter("@OneTimePassword", dto.OneTimePassword);
+            command.Parameters.Add(OneTimePasswordParam);
+
+            int rows = command.ExecuteNonQuery();
+            Connection.Close();
+
+            return rows;
         }
     }
 }
