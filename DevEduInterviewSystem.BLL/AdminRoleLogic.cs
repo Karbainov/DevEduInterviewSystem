6edada9152e8c0a6f79e2e99187712b9f70f206d
Di <diana.iancheva@gmail.com>
2020-08-18 13:48:30 +0300

    Methods update

--- DevEduInterviewSystem.BLL/AdminRoleLogic.cs
+++ DevEduInterviewSystem.BLL/AdminRoleLogic.cs
@@ -15,27 +15,32 @@ namespace DevEduInterviewSystem.BLL
         //}
 
         #region Methods for adding fields in system tables
-        public void AddRole(RoleDTO role, RoleCRUD crud)
+        public void AddRole(RoleDTO role)
         {
+            RoleCRUD crud = new RoleCRUD();
             crud.Add(role);
         }
-        public void AddStage(StageDTO stage, StageCRUD crud)
+        public void AddStage(StageDTO stage)
         {
+            StageCRUD crud = new StageCRUD();
             crud.Add(stage);
         }
-        public void AddStatus(StatusDTO status, StatusCRUD crud)
+        public void AddStatus(StatusDTO status)
         {
+            StatusCRUD crud = new StatusCRUD();
             crud.Add(status);
         }
-        public void InsertCity(CityDTO city, CityCRUD crud)
+        public void InsertCity(CityDTO city)
         {
             CityCRUD crud = new CityCRUD();
             crud.Add(city);
         }
         #endregion
 
-        public void AddNewUser(UserDTO userDTO, UserCRUD user, UserRoleDTO roleDTO, UserRoleCRUD role)
+        public void AddNewUser(UserDTO userDTO, UserRoleDTO roleDTO)
         {
+            UserCRUD user = new UserCRUD();
+            UserRoleCRUD role = new UserRoleCRUD();
             user.Add(userDTO);
             role.UpdateByID(roleDTO);
         }
