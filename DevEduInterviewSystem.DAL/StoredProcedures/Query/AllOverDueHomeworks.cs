2646bf90a686f1bff72bd1f6ef332608a5dccb46
Alesksandr <haman282828@yandex.ru>
2020-08-18 20:33:56 +0300

    Add AllOverdueHomeworks DTO & Query

---
+++ DevEduInterviewSystem.DAL/StoredProcedures/Query/AllOverDueHomeworks.cs
@@ -0,0 +1,52 @@
+﻿using DevEduInterviewSystem.DAL.DTO.QuereDTO;
+using DevEduInterviewSystem.DAL.Shared;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+
+namespace DevEduInterviewSystem.DAL.StoredProcedures.Query.AllOverdueHomework
+{
+    public class AllOverdueHomeworks
+    {
+        public List<AllOverdueHomeworksDTO> GetAllOverdueHomeworks(DateTime dateTime)
+        {
+            SqlConnection Connection = new SqlConnection(ConnectionSingleTone.GetInstance().ConnectionString);
+            Connection.Open();
+            DateTime dateCurrent = DateTime.Now;
+            SqlCommand command = ReferenceToProcedure("AllOverdueHomework", Connection);
+            SqlParameter currentDateParam = new SqlParameter("@DateCurrent", new DateTime(2020, 07, 20, 18, 30, 00));
+            command.Parameters.Add(currentDateParam);
+
+            List<AllOverdueHomeworksDTO> allOverdueHomeworks = new List<AllOverdueHomeworksDTO>();
+
+            SqlDataReader reader = command.ExecuteReader();
+            if (reader.HasRows)
+            {
+                while (reader.Read())
+                {
+                    AllOverdueHomeworksDTO allOverdueHomework = new AllOverdueHomeworksDTO()
+                    {
+                        /*CandidateID = (int)reader["CandidateID"],*/
+                        HomeWorkDate = (DateTime)reader["HomeWorkDate"],
+                        CandidateFirstName = (string)reader["CandidateFirstName"],
+                        CandidateLastName = (string)reader["CandidateLastName"],
+                        HomeWorkStatus = (string)reader["HomeWorkStatus"],
+                    };
+
+                    allOverdueHomeworks.Add(allOverdueHomework);
+                }
+            }
+            reader.Close();
+            Connection.Close();
+            return allOverdueHomeworks;
+
+        }
+        private SqlCommand ReferenceToProcedure(string sqlExpression, SqlConnection connection)
+        {
+            SqlCommand command = new SqlCommand(sqlExpression, connection);
+            command.CommandType = System.Data.CommandType.StoredProcedure;
+
+            return command;
+        }
+    }
+}
--- DevEduInterviewSystem.DataBase/StoredProcedures/AllOverdueTests.sql
+++ DevEduInterviewSystem.DataBase/StoredProcedures/AllOverdueTests.sql
@@ -2,6 +2,6 @@
 @DateCurrent datetime2
 AS
 SELECT HW.[HomeworkDate] as [HomeworkDate], C.[FirstName] as [CandidateFirstName], C.[LastName] as [CandidateLastName], TSt.[Name] as [TestStatusName]
-FROM [dbo].[Homework] as HW
-JOIN [dbo].[Candidate] as C on HW.[CandidateID] = C.[ID]
-JOIN [dbo].[TestStatus] as TSt on HW.[HomeworkStatusID] = TSt.[ID]
+FROM dbo.[Homework] as HW
+JOIN dbo.[Candidate] as C on HW.[CandidateID] = C.[ID]
+JOIN dbo.[TestStatus] as TSt on HW.[HomeworkStatusID] = TSt.[ID]
