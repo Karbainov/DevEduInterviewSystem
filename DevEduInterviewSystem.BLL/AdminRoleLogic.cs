6147330994906d8e397df42c0845b6e452f56ad8
Di <diana.iancheva@gmail.com>
2020-08-18 13:43:53 +0300

    Added methods for admin logic

--- DevEduInterviewSystem.BLL/AdminRoleLogic.cs
+++ DevEduInterviewSystem.BLL/AdminRoleLogic.cs
@@ -7,64 +7,37 @@ using System.Text;
 
 namespace DevEduInterviewSystem.BLL
 {
-    public class AdminRoleLogic : ARoleLogic
+    public class AdminRoleLogic : IRoleLogic
     {
+        //public SqlConnection Connection { get ; set; }
+        //public void GetConnection()
+        //{
+        //}
+
+        #region Methods for adding fields in system tables
+        public void AddRole(RoleDTO role, RoleCRUD crud)
+        {
+            crud.Add(role);
+        }
+        public void AddStage(StageDTO stage, StageCRUD crud)
+        {
+            crud.Add(stage);
+        }
+        public void AddStatus(StatusDTO status, StatusCRUD crud)
+        {
+            crud.Add(status);
+        }
         public void InsertCity(CityDTO city, CityCRUD crud)
         {
             crud.Add(city);
         }
-        public void AddNewUser(UserDTO user, UserRoleDTO role)
+        #endregion
+
+        public void AddNewUser(UserDTO userDTO, UserCRUD user, UserRoleDTO roleDTO, UserRoleCRUD role)
         {
-            AddUser(user);
-            UpdateUserRole(role);
-
+            user.Add(userDTO);
+            role.UpdateByID(roleDTO);
         }
-        private int AddUser(UserDTO dto)
-        {
-            Connection.Open();
-            SqlCommand command = ReferenceToProcedure("AddUser");
 
-            SqlParameter LoginParam = new SqlParameter("@Login", dto.Login);
-            command.Parameters.Add(LoginParam);
-
-            SqlParameter PasswordParam = new SqlParameter("@Password", dto.Password);
-            command.Parameters.Add(PasswordParam);
-
-            SqlParameter FirstNameParam = new SqlParameter("@FirstName", dto.FirstName);
-            command.Parameters.Add(FirstNameParam);
-
-            SqlParameter LastNameParam = new SqlParameter("@LastName", dto.LastName);
-            command.Parameters.Add(LastNameParam);
-
-            SqlParameter IsDeletedParam = new SqlParameter("@IsDeleted", dto.IsDeleted);
-            command.Parameters.Add(IsDeletedParam);
-
-            command.ExecuteNonQuery();
-
-            SqlCommand returnCurrentID = new SqlCommand("SELECT MAX([ID]) FROM dbo.[User]", Connection);
-            int count = (int)returnCurrentID.ExecuteScalar();
-
-            Connection.Close();
-            return count;
-        }
-        private int UpdateUserRole(UserRoleDTO dto)
-        {
-            Connection.Open();
-            SqlCommand command = ReferenceToProcedure("UpdateUserRoleByID");
-
-            SqlParameter IDParam = new SqlParameter("@ID", dto.ID);
-            command.Parameters.Add(IDParam);
-
-            SqlParameter UserID = new SqlParameter("@UserID", dto.UserID);
-            command.Parameters.Add(UserID);
-
-            SqlParameter RoleID = new SqlParameter("@RoleID", dto.RoleID);
-            command.Parameters.Add(RoleID);
-
-            int rows = command.ExecuteNonQuery();
-            Connection.Close();
-
-            return rows;
-        }
     }
 }
