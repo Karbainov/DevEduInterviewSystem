884cbb917b1af2d1069c7f7a40ad42ae81603a71
Denis <denis,matveev@yahoo.com>
2020-08-19 23:34:18 +0300

    Add UpdateCandidatePersonalInfo logic

--- DevEduInterviewSystem.BLL/ManagerRoleLogic.cs
+++ DevEduInterviewSystem.BLL/ManagerRoleLogic.cs
@@ -9,5 +9,14 @@ namespace DevEduInterviewSystem.BLL
 {
     public class ManagerRoleLogic : IRoleLogic
     {
+        // Менеджер может обновить данные кандидата и personal info.
+        public void UpdateCandidatePersonalInfo(CandidateDTO candidateDTO, CandidatePersonalInfoDTO candidatePersonalInfoDTO)
+        {
+            CandidateCRUD candidate = new CandidateCRUD();
+            candidate.UpdateByID(candidateDTO);
+
+            CandidatePersonalInfoCRUD candidatePersonalInfo = new CandidatePersonalInfoCRUD();
+            candidatePersonalInfo.UpdateByID(candidatePersonalInfoDTO);
+        }
     }
 }
