aa398aa6a690dccaec6a3e594e11a096b8ca9b72
Di <diana.iancheva@gmail.com>
2020-08-22 00:41:15 +0300

    Added user input model and controller for adding new user

--- DevEduInterviewSystem.API/Controllers/CandidateController.cs
+++ DevEduInterviewSystem.API/Controllers/CandidateController.cs
@@ -43,7 +43,7 @@ namespace DevEduInterviewSystem.API.Controllers
             }
             else
             {
-                return BadRequest("Fields meesing");
+                return BadRequest("Fields missing");
             }
         }
         [HttpGet("k")]
--- DevEduInterviewSystem.API/Controllers/UserController.cs
+++ DevEduInterviewSystem.API/Controllers/UserController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using DevEduInterviewSystem.API.Models.Input;
 using DevEduInterviewSystem.BLL;
 using DevEduInterviewSystem.DAL.DTO;
 using DevEduInterviewSystem.DAL.StoredProcedures.CRUD;
@@ -19,6 +20,28 @@ namespace DevEduInterviewSystem.API.Controllers
         private ManagerRoleLogic _manager = new ManagerRoleLogic();
         private AdminRoleLogic _admin = new AdminRoleLogic();
 
+        [HttpPost("Users")]
+        public IActionResult AddUser(UserInputModel user)
+        {
+            if (new RoleCRUD().SelectByID((int)user.RoleID) == null)
+            {
+                return new NotFoundObjectResult("Role not found");
+            }
+            if ((user.UserDTO.FirstName != null
+                && user.UserDTO.LastName != null)
+                && user.UserDTO.Password != null
+                && user.RoleID != null)
+            {
+                user.UserDTO.IsDeleted = false;
+                _admin.AddNewUser(user.UserDTO, (int)user.RoleID);
+                return new OkResult();
+            }
+            else
+            {
+                return BadRequest("Fields meesing");
+            }
+        }
+
         [HttpDelete("Users")]
         public IActionResult DeleteUser(int? userID)
         {
@@ -30,6 +53,8 @@ namespace DevEduInterviewSystem.API.Controllers
             _admin.DeleteUser((int)userID);
             return new OkResult();
         }
+
+       
 
     }
 }
