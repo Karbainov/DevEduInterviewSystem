d8e4568cae7e729fa2dee774f4f913652342b884
Di <diana.iancheva@gmail.com>
2020-08-22 23:08:23 +0300

    Get roles method

---
+++ DevEduInterviewSystem.DAL/StoredProcedures/Query/User/GetRolesByUserID.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DevEduInterviewSystem.DAL.StoredProcedures.Query.User
+{
+    class GetRolesByUserID
+    {
+    }
+}
---
+++ DevEduInterviewSystem.DataBase/StoredProcedures/User/GetRolesByUserID.sql
@@ -0,0 +1,7 @@
+﻿CREATE PROCEDURE [dbo].[GetRolesByUserID]
+@ID int  
+AS 
+SELECT R.[TypeOfRole] 
+FROM dbo.[ROLE] AS R
+INNER JOIN dbo.[User_Role] AS UR ON UR.[RoleID] = UR.[UserID]
+WHERE (@ID = UR.[UserID])
