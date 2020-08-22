36f8c613cedea9287de343c31b10e082e9dad1b2
Anastasia <anastasila.mik@yandex.ru>
2020-08-22 22:27:21 +0300

    Create mocks for all tables and used it in tests
    Also fixed bugs in procedures

---
+++ DevEduInterviewSystem.DAL.Tests/Mocks/HomeworkStatusDTOMock.cs
@@ -0,0 +1,19 @@
+﻿using DevEduInterviewSystem.DAL.DTO;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DevEduInterviewSystem.DAL.Tests
+{
+    public class HomeworkStatusDTOMock : IEnumerable
+    {
+        public IEnumerator GetEnumerator()
+        {
+            yield return new HomeworkStatusDTO(1, "Сдана");
+            yield return new HomeworkStatusDTO(2, "Пропущен срок сдачи");
+            yield return new HomeworkStatusDTO(3, "Отправлена кандидату");
+        }
+
+    }
+}
