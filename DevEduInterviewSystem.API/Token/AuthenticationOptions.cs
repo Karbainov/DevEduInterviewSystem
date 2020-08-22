a8becdc2c7dcf170d6d03f75560319881b2d13ef
Di <diana.iancheva@gmail.com>
2020-08-22 03:12:45 +0300

    Added set up for token

---
+++ DevEduInterviewSystem.API/Token/AuthenticationOptions.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevEduInterviewSystem.API.Token
+{
+    public struct AuthenticationOptions
+    {
+        public const string ISSUER = "MyAuthServer"; 
+        public const string AUDIENCE = "MyUsers"; 
+        const string KEY = "mysupersecret_secretkey!123";   
+        public const int LIFETIME = 8; 
+        public static SymmetricSecurityKey GetSymmetricSecurityKey()
+        {
+            return new SymmetricSecurityKey(Encoding.ASCII.GetBytes(KEY));
+        }
+    }
+}
