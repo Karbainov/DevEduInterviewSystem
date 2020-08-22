266cf9ceb28859d08b24c9c2bc0e2435af4dfff8
AndreyKarlukov <karl911291@yandex.ru>
2020-08-22 12:36:40 +0300

    Add feedback controller

---
+++ DevEduInterviewSystem.API/Controllers/FeedbackController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DevEduInterviewSystem.API.Models.Input;
+using DevEduInterviewSystem.BLL;
+using DevEduInterviewSystem.DAL.StoredProcedures.CRUD;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DevEduInterviewSystem.API.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class FeedbackController : Controller
+    {
+        private PhoneOperatorRoleLogic _phoneOperator = new PhoneOperatorRoleLogic();
+        private TeacherRoleLogic _teacher = new TeacherRoleLogic();
+        private ManagerRoleLogic _manager = new ManagerRoleLogic();
+
+        [HttpPost]
+        public IActionResult CreateFeedback(FeedbackInputModel feedbackInputModel)
+        {
+            if (new UserCRUD().SelectByID((int)feedbackInputModel.feedbackDTO.UserID) == null)
+            {
+                return new NotFoundObjectResult("User not found");
+            }
+            if (new StageChangedCRUD().SelectByID((int)feedbackInputModel.feedbackDTO.StageChangedID) == null)
+            {
+                return new NotFoundObjectResult("StageChangedID not found");
+            }
+            if (feedbackInputModel.feedbackDTO.StageChangedID > 0 &&
+               feedbackInputModel.feedbackDTO.UserID > 0 &&
+               feedbackInputModel.feedbackDTO.Message != null)
+            {
+                feedbackInputModel.feedbackDTO.TimeFeedback = DateTime.Now;
+                _teacher.AddFeedback(feedbackInputModel.feedbackDTO);
+                return new OkResult();
+            }
+            else
+            {
+                return BadRequest("Field meesing");
+            }
+        }
+
+        //[HttpGet]
+        //public IActionResult GetAllFeedback
+    }
+}
--- DevEduInterviewSystem.DAL.Tests/OneUserRolesTest.cs
+++ DevEduInterviewSystem.DAL.Tests/OneUserRolesTest.cs
@@ -18,59 +18,59 @@ namespace DevEduInterviewSystem.DAL.Tests
 {
     public class OneUserRolesTest
     {
-        UserDTO userDTO;
-        RoleDTO roleDTO;
-        RoleDTO role2DTO;
-        UserRoleDTO userRoleDTO;
-        OneUserRolesDTO oneUserRolesDTO;
+    //    UserDTO userDTO;
+    //    RoleDTO roleDTO;
+    //    RoleDTO role2DTO;
+    //    UserRoleDTO userRoleDTO;
+    //    OneUserRolesDTO oneUserRolesDTO;
 
-        UserCRUD user = new UserCRUD();
-        RoleCRUD role = new RoleCRUD();
-        RoleCRUD role2 = new RoleCRUD();
-        UserRoleCRUD userRole = new UserRoleCRUD();
+    //    UserCRUD user = new UserCRUD();
+    //    RoleCRUD role = new RoleCRUD();
+    //    RoleCRUD role2 = new RoleCRUD();
+    //    UserRoleCRUD userRole = new UserRoleCRUD();
 
-        int userID;
-        int roleID;
-        int role2ID;
-        int userRoleID;
+    //    int userID;
+    //    int roleID;
+    //    int role2ID;
+    //    int userRoleID;
 
-        [SetUp]
-        public void Setup()
-        {
-            SqlConnection connection = new SqlConnection(ConnectionSingleTone.GetInstance().ConnectionString);
+    //    [SetUp]
+    //    public void Setup()
+    //    {
+    //        SqlConnection connection = new SqlConnection(ConnectionSingleTone.GetInstance().ConnectionString);
 
-            userDTO = new UserDTO(1, "Login", "Pass", "Polina", "Polikarpovna", false);
-            userID = user.Add(userDTO);
+    //        userDTO = new UserDTO(1, "Login", "Pass", "Polina", "Polikarpovna", false);
+    //        userID = user.Add(userDTO);
 
-            roleDTO = new RoleDTO(1, "Teacher");
-            roleID = role.Add(roleDTO);
-            role2DTO = new RoleDTO(2, "Manager");
-            role2ID = role.Add(role2DTO);
+    //        roleDTO = new RoleDTO(1, "Teacher");
+    //        roleID = role.Add(roleDTO);
+    //        role2DTO = new RoleDTO(2, "Manager");
+    //        role2ID = role.Add(role2DTO);
 
-            List<RoleDTO> roles = new List<RoleDTO>();
-            roles.Add(roleDTO);
-            roles.Add(role2DTO);
+    //        List<RoleDTO> roles = new List<RoleDTO>();
+    //        roles.Add(roleDTO);
+    //        roles.Add(role2DTO);
 
-            userRoleDTO = new UserRoleDTO(userID, roleID);
-            userRoleID = userRole.Add(userRoleDTO);
+    //        userRoleDTO = new UserRoleDTO(userID, roleID);
+    //        userRoleID = userRole.Add(userRoleDTO);
 
-            oneUserRolesDTO = new OneUserRolesDTO("Login", "Polina", "Polikarpovna", roles);
-        }
-        public void TearDown()
-        {
-            user.DeleteByID(userID);
-        }
+    //        oneUserRolesDTO = new OneUserRolesDTO("Login", "Polina", "Polikarpovna", roles);
+    //    }
+    //    public void TearDown()
+    //    {
+    //        user.DeleteByID(userID);
+    //    }
 
-        [TestCase(true)]
-        public void Test1(bool expected)
-        {
-            OneUserRolesQuery query = new OneUserRolesQuery();
+    //    [TestCase(true)]
+    //    public void Test1(bool expected)
+    //    {
+    //        OneUserRolesQuery query = new OneUserRolesQuery();
 
-            bool actual = oneUserRolesDTO.Equals(query.SelectUserWithAllRoles(userID));
+    //        bool actual = oneUserRolesDTO.Equals(query.SelectUserWithAllRoles(userID));
 
-            Assert.AreEqual(expected, actual);
+    //        Assert.AreEqual(expected, actual);
 
-        }
+      // }
 
     }
 }
