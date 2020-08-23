5fe76c34ba42a11517530ee1627c0de25c325696
Roman <ryzo95@mail.ru>
2020-08-23 19:24:02 +0300

    Edit Dapper

--- DevEduInterviewSystem.DAL/StoredProcedures/Query/AllInformationAboutTheCandidateByIDProcedure.cs
+++ DevEduInterviewSystem.DAL/StoredProcedures/Query/AllInformationAboutTheCandidateByIDProcedure.cs
@@ -19,22 +19,26 @@ namespace DevEduInterviewSystem.DAL.StoredProcedures.Query
         {           
             IDbConnection connection = new SqlConnection(ConnectionSingleTone.GetInstance().ConnectionString);
             var procedure = "[AllInformationAboutTheCandidateByID]";
-        
-            var allInfo = connection.Query< AllInformationAboutTheCandidateByIDDTO, FeedbackDTO, AllInformationAboutTheCandidateByIDDTO>(procedure,(info, feedback) => 
+
+            List<FeedbackDTO> feedbackDTOs = new List<FeedbackDTO>();
+
+           var allInfo = connection.Query< AllInformationAboutTheCandidateByIDDTO, FeedbackDTO,AllInformationAboutTheCandidateByIDDTO>(procedure,(info, feedback) => 
             {
+               
                 if (info.FeedBack == null)
                 {
                     info.FeedBack = new List<FeedbackDTO>();
                 }
-                                
-                if (!info.FeedBack.Contains(feedback))
+
+
+                if (!info.FeedBack.Any(x => feedback.Equals(x)))
                 {
                     info.FeedBack.Add(feedback);
                 }
-                
+
                 return info;
             }, new {ID},
-          splitOn: "TypeOfStage,TypeOfStatus,CityName,Message,CourseName,GroupName,MaritalStatus",
+         splitOn: "Message",
             commandType: CommandType.StoredProcedure
             ).FirstOrDefault();
 
