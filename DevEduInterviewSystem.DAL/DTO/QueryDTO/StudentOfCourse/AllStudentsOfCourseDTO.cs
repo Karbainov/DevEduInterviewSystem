1441f32dae10db3e99f1eeae0d22a724fc281afb
Mikhail <gooddok99@gmail.com>
2020-08-24 03:56:15 +0300

    AllStudentsOfCourseTest added + minor updates and fixes for AllStudentsOfCourseDTO and CRUD

--- DevEduInterviewSystem.DAL/DTO/QueryDTO/StudentOfCourse/AllStudentsOfCourseDTO.cs
+++ DevEduInterviewSystem.DAL/DTO/QueryDTO/StudentOfCourse/AllStudentsOfCourseDTO.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using DevEduInterviewSystem.DAL.DTO.QueryDTO.CandidateSelectionProcessInfoDTO;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -6,7 +7,7 @@ namespace DevEduInterviewSystem.DAL.DTO.QuereDTO.StudentsOfCourse
 {
     public class AllStudentsOfCourseDTO : IDTO
     {
-        public string Name {get;set;}
+        public string Name { get; set; }
         public string StudentFirstName { get; set; }
         public string StudentLastName { get; set; }
 
@@ -20,7 +21,17 @@ namespace DevEduInterviewSystem.DAL.DTO.QuereDTO.StudentsOfCourse
             Name = name;
             StudentFirstName = firstName;
             StudentLastName = lastName;
-            
+
+        }
+        public override bool Equals(object obj)
+        {
+            if (obj.GetType() != this.GetType()) return false;
+
+            AllStudentsOfCourseDTO dto = (AllStudentsOfCourseDTO)obj;
+            return (Name == dto.Name
+                && this.StudentFirstName == dto.StudentFirstName
+                && this.StudentLastName == dto.StudentLastName);
+
         }
     }
 }
