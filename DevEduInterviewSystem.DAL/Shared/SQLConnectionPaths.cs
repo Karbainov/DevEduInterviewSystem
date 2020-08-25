d9ee657811c199e8b9196b34b10bce0d3f300160
di_locksley <diana.iancheva@gmail.com>
2020-08-25 13:20:18 +0300

    added group check for course

--- DevEduInterviewSystem.DAL/Shared/SQLConnectionPaths.cs
+++ DevEduInterviewSystem.DAL/Shared/SQLConnectionPaths.cs
@@ -8,8 +8,10 @@ namespace DevEduInterviewSystem.DAL.Shared
     {
         //public const string MainConnectionString = @"Data Source=DESKTOP-1NDKC00;Initial Catalog=DevEduInterviewSystem.DataBase;Integrated Security=True; Data Source=(local)"; 
         //public const string LocalConnectionString = @"Data Source=HP;Initial Catalog=DevEduInterviewSystem.DataBaseTest;Integrated Security=True; Data Source=(local)"; 
-        public const string TestConnectionString = @"Data Source = 31.31.196.234; Initial Catalog = u1093787_InterviewSystem; Persist Security Info=True;User ID = u1093787_usr; Password = Clms81$5";
-        public const string MainConnectionString = @"Data Source = 31.31.196.234; Initial Catalog = u1093787_InterviewSystem; Persist Security Info=True;User ID = u1093787_usr; Password = Clms81$5";
+        public const string TestConnectionString = @"Data Source = 31.31.196.234; Initial Catalog = u1093787_InterviewSystem; 
+Persist Security Info=True;User ID = u1093787_usr; Password = ";
+        public const string MainConnectionString = @"Data Source = 31.31.196.234; Initial Catalog = u1093787_InterviewSystem; 
+Persist Security Info=True;User ID = u1093787_usr; Password = ";
 
     }
 }
